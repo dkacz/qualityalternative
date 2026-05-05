8295636b3d3a76b49d154471c73f2854054f6a70
Dominik Kaczmarski <omare@mac-studio.home>
2026-05-05 16:29:43 +0200

    Bump Android version for Sprint 16 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.qualityalternative.app"
         minSdk = 29
         targetSdk = 36
-        versionCode = 13
-        versionName = "0.8.1-alpha"
+        versionCode = 14
+        versionName = "0.9.0-alpha"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
